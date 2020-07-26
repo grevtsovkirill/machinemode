6cfe02124a97cdffa016752c3c89171abe86a4bc
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-26 17:18:29 +0200

    load/retrain model. make prediction, show/save report

--- machinemode.py
+++ machinemode.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import xgboost
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix, plot_confusion_matrix
+import pickle
+import matplotlib.pyplot as plt
 
 def load_data(name = 'data_case_study.csv',path = './'):
     data = pd.read_csv("data_case_study.csv")
@@ -20,7 +23,7 @@ def data_prep(ds,varlist=[]):
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
     return x_train, x_test, y_train, y_test, n_class
 
-def build_model(opt='def'):
+def build_model(x,y,opt='def'):
     param = {}
     # use softmax multi-class classification
     param['objective'] = 'multi:softmax'
@@ -30,14 +33,29 @@ def build_model(opt='def'):
     param['nthread'] = 4
     param['num_class'] = 6
     model = xgboost.XGBClassifier(**param)
+
+    if opt=='def':
+        model.fit(x,y)
+        with open('model_xgb_def.pickle', 'wb') as f:
+            pickle.dump(model, f)
+
+    elif opt=='load':
+        with open('model_xgb_def.pickle', 'rb') as f:
+            model = pickle.load(f)
+    
     return model
     
 def main():
     data = load_data()
     x_train, x_test, y_train, y_test, n_class = data_prep(data)
-    model = build_model()
-    model.fit(x_train,y_train)
+    model = build_model(x_train,y_train,'load')    
     
+
+    predictions = model.predict(x_test)
+    print(classification_report(y_test, predictions))
+    print(confusion_matrix(y_test, predictions))
+    plot_confusion_matrix(model, x_test, y_test)
+    plt.savefig("Plots/conf_m.png", transparent=True)
     
 if __name__ == "__main__":
     main()
