4de0d31dcc9a54adbbabf1e9f4cdf49d94281dfc
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-26 17:04:20 +0200

    define model params and build classifier

--- machinemode.py
+++ machinemode.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import xgboost
 from sklearn.model_selection import train_test_split
 
 def load_data(name = 'data_case_study.csv',path = './'):
@@ -15,13 +15,29 @@ def data_prep(ds,varlist=[]):
         varlist = [it for it in varlist0 if it not in todel]
 
     X = ds[varlist]
+    n_class = len(Y.value_counts().index)
     
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
-    return x_train, x_test, y_train, y_test
+    return x_train, x_test, y_train, y_test, n_class
+
+def build_model(opt='def'):
+    param = {}
+    # use softmax multi-class classification
+    param['objective'] = 'multi:softmax'
+    # scale weight of positive examples
+    param['eta'] = 0.1
+    param['max_depth'] = 4
+    param['nthread'] = 4
+    param['num_class'] = 6
+    model = xgboost.XGBClassifier(**param)
+    return model
     
 def main():
     data = load_data()
-    x_train, x_test, y_train, y_test = data_prep(data)
+    x_train, x_test, y_train, y_test, n_class = data_prep(data)
+    model = build_model()
+    model.fit(x_train,y_train)
+    
     
 if __name__ == "__main__":
     main()
