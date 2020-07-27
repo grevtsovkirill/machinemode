1c2d04718baba821ac06a34aa084dd6391b908a9
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-27 20:46:51 +0200

    update setyp with requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    requirements = [line for line in f if not line.startswith("#")]
+    
 setup(
     name='machinemode',
+    version="0.1",
+    author="Kirill Grevtsov",
+    author_email="grevtsovkirill@gmail.com",
+    description="build a model for classifying the state of machine using the measurements from the sensors",
     packages=["machinemode"],
     entry_points={
         "console_scripts": ["machinemode = machinemode.__main__:main"]},
-    install_requires=[],
+    install_requires=requirements,
 )
