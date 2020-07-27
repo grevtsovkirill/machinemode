5f5ca7ce169c8d08251bc0b6bc105c296774143c
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-27 21:08:18 +0200

    create data class

--- machinemode/__main__.py
+++ machinemode/__main__.py
@@ -2,11 +2,13 @@ from machinemode import dataprep,modeldef
 
     
 def main():
-    data = dataprep.load_data()
-    x_train, x_test, y_train, y_test, n_class = dataprep.data_prep(data)
-    model = modeldef.build_model(x_train,y_train,'load')    
+
+    data = dataprep.MachineData()
+    data.data_prep()
+
+    model = modeldef.build_model(data.x_train,data.y_train,'load')    
     
-    predictions = model.predict(x_test)
-    modeldef.model_perf(model,x_test,y_test,predictions)
+    predictions = model.predict(data.x_test)
+    modeldef.model_perf(model,data.x_test,data.y_test,predictions)
 if __name__ == "__main__":
     main()
--- machinemode/dataprep.py
+++ machinemode/dataprep.py
@@ -1,22 +1,33 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+class MachineData:
+    def __init__(self, data_dir = './', filename = 'data_case_study.csv'):
+        self.data_dir=data_dir
+        self.filename=filename
 
 
-def load_data(name = 'data_case_study.csv',path = './'):
-    data = pd.read_csv("data_case_study.csv")
-    data = data.fillna(0)
-    return data
 
-def data_prep(ds,varlist=[]):
-    Y = ds['activity']
-    if len(varlist)==0:
-        varlist0 = list(ds.select_dtypes(include='number').columns)
-        todel = ['activity','timestamp','Unnamed: 0']
-        varlist = [it for it in varlist0 if it not in todel]
-
-    X = ds[varlist]
-    n_class = len(Y.value_counts().index)
-    
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
-    return x_train, x_test, y_train, y_test, n_class
+    def load_data(self):
+        data = pd.read_csv(self.data_dir+self.filename)
+        data = data.fillna(0)
+        self.cleandata = data
+        
+        
+    def data_prep(self,varlist=[]):
+        self.load_data()
+        Y = self.cleandata['activity']
+        if len(varlist)==0:
+            varlist0 = list(self.cleandata.select_dtypes(include='number').columns)
+            todel = ['activity','timestamp','Unnamed: 0']
+            varlist = [it for it in varlist0 if it not in todel]
+        
+        X = self.cleandata[varlist]
+        n_class = len(Y.value_counts().index)
+        
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
+        #return x_train, x_test, y_train, y_test, n_class
+        self.x_test = x_test
+        self.y_test = y_test
+        self.x_train = x_train
+        self.y_train = y_train
