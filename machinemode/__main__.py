b25492e0de9023e6348454b95f83e1cfc962f854
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-28 14:23:39 +0200

    add description to class call

--- machinemode/__main__.py
+++ machinemode/__main__.py
@@ -4,11 +4,15 @@ from machinemode import dataprep,modeldef
 def main():
 
     data = dataprep.MachineData()
-    data.data_prep()
+    '''
+    Prepare data for training: split train/test
+    '''
+    data.data_train()
 
     model = modeldef.get_model(data.x_train,data.y_train,data.n_class,'load')       
     predictions = model.predict(data.x_test)
     modeldef.model_perf(model,data.x_test,data.y_test,predictions)
+
     
 if __name__ == "__main__":
     main()
