a6adab4c0312827cb5f6f65506bef6f5613f8561
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-28 14:23:08 +0200

    prep data for training and for application

--- machinemode/dataprep.py
+++ machinemode/dataprep.py
@@ -4,9 +4,10 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 class MachineData:
-    def __init__(self, data_dir = './', filename = 'data_case_study.csv'):
+    def __init__(self, data_dir = './', filename = 'data_case_study.csv',varlist=[]):
         self.data_dir=data_dir
         self.filename=filename
+        self.varlist=varlist
 
 
 
@@ -16,25 +17,33 @@ class MachineData:
         self.cleandata = data
         
         
-    def data_prep(self,varlist=[]):
+    def data_prep(self):
         self.load_data()
         Y = self.cleandata['activity']
+        self.Y = Y
         n_class = len(Y.value_counts().index)
         self.n_class = n_class
-        if len(varlist)==0:
+        if len(self.varlist)==0:
             varlist0 = list(self.cleandata.select_dtypes(include='number').columns)
             todel = ['activity','timestamp','Unnamed: 0']
-            varlist = [it for it in varlist0 if it not in todel]
-        
+            varlist = [it for it in varlist0 if it not in todel]        
         X = self.cleandata[varlist]
-        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 123)
-        self.fracs(Y,y_test,y_train)
+        self.X = X
+        
+    def data_train(self):
+        self.data_prep()
+        x_train, x_test, y_train, y_test = train_test_split(self.X, self.Y, test_size = 0.3, random_state = 123)
+        self.fracs(self.Y,y_test,y_train)
         
         self.x_test = x_test
         self.y_test = y_test
         self.x_train = x_train
         self.y_train = y_train
 
+    def data_apply(self):
+        self.data_prep()
+
+    
     def fracs(self, y, y_test,y_train):
         act_type= list(y.value_counts().index)
         df = y.value_counts().to_frame(name="tot")
