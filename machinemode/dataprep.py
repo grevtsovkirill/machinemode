3cd205d44c9aa3e5d2fe7e8017ff7bd22e15592d
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-28 14:07:24 +0200

    check split fractions

--- machinemode/dataprep.py
+++ machinemode/dataprep.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import os
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 class MachineData:
     def __init__(self, data_dir = './', filename = 'data_case_study.csv'):
@@ -25,9 +27,21 @@ class MachineData:
             varlist = [it for it in varlist0 if it not in todel]
         
         X = self.cleandata[varlist]
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 123)
+        self.fracs(Y,y_test,y_train)
         
-        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
         self.x_test = x_test
         self.y_test = y_test
         self.x_train = x_train
         self.y_train = y_train
+
+    def fracs(self, y, y_test,y_train):
+        act_type= list(y.value_counts().index)
+        df = y.value_counts().to_frame(name="tot")
+        df=df/len(y)
+        df['test'] = y_test.value_counts()/len(y_test)
+        df['train'] = y_train.value_counts()/len(y_train)
+        df.plot.barh().invert_yaxis()
+        if not os.path.exists('Plots'):
+            os.makedirs('Plots')
+        plt.savefig("Plots/split.png", transparent=True)
