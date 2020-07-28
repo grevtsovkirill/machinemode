66b386b6ba671866f1661c6368def576f01721ee
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-07-28 14:06:58 +0200

    remove intend

--- machinemode/__main__.py
+++ machinemode/__main__.py
@@ -6,9 +6,9 @@ def main():
     data = dataprep.MachineData()
     data.data_prep()
 
-    model = modeldef.get_model(data.x_train,data.y_train,data.n_class,'load')    
-    
+    model = modeldef.get_model(data.x_train,data.y_train,data.n_class,'load')       
     predictions = model.predict(data.x_test)
     modeldef.model_perf(model,data.x_test,data.y_test,predictions)
+    
 if __name__ == "__main__":
     main()
